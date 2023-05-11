4b1c0285bad395bafa45e86e6689429143e686c2
elpanaflork <lablanca.miguelet36@gmail.com>
2023-05-11 15:53:16 -0300

    inicio del snake v2

---
+++ main.js
@@ -0,0 +1,11 @@
+function establecerDireccion(keycode){
+    
+    if(keycode ==  || keycode == )
+    {
+        return ()
+    }
+    arriba = 38 || 87;
+    abajo = 40 || 83;
+    derecha = 39 || 68;
+    izquierda = 37 || 65;
+} 
